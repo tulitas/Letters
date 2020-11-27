5ae8256c8c4c02dde4ffbee2c414045b642d597f
sergej <tulitas1@gmail.com>
2020-11-27 23:48:34 +0200

    added tests

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,26 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    private static int num;
-    public static void main(String[] args) throws FileNotFoundException {
+    private static Scanner input;
+    private static final String filePath = "C:/Users/user/IdeaProjects/Letters/src/resources/output.txt";
+    private static final int[] count = new int[26];
+    private static int totalLetters = 0;
 
-        Scanner input = new Scanner(new File("C:/Users/User/IdeaProjects/Letters/src/resources/output.txt"));
-        int[] count = new int[26];
+    public static void main(String[] args) throws FileNotFoundException {
+        takeFile();
+    }
+
+    public static void takeFile() {
+        try {
+            input = new Scanner(new File(filePath));
+        } catch (FileNotFoundException e) {
+            System.out.println("Can`t find file");
+        }
+        takeLetters();
+    }
+
+    public static void takeLetters() {
+        int result = 0;
         while (input.hasNextLine()) {
             String answer = input.nextLine();
             answer = answer.toLowerCase();
@@ -15,22 +30,42 @@ public class Main {
                 if ((character >= 'a') && (character <= 'z')) {
                     count[character - 'a']++;
                 }
+//                totalLetters++;
             }
         }
+        makeResult();
+        takeAllLeters();
+    }
+
+    public static void takeAllLeters() {
+        System.out.println("Total letters: " + totalLetters);
+    }
+
+    private static void makeResult() {
         for (int i = 0; i < 26; i++) {
-            num = 25;
             System.out.print((char) (i + 'a'));
             System.out.println(": " + count[i]);
-
         }
         input.close();
     }
 
-    public static int getNum() {
-        return num;
+    public static Scanner getInput() {
+        return input;
     }
 
-    public static void setNum(int num) {
-        Main.num = num;
+    public static void setInput(Scanner input) {
+        Main.input = input;
+    }
+
+    static String getFilePath() {
+        return filePath;
+    }
+
+    public static int getTotalLetters() {
+        return totalLetters;
+    }
+
+    public static void setTotalLetters(int totalLetters) {
+        Main.totalLetters = totalLetters;
     }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,14 +1,20 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-
+import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    void main() throws FileNotFoundException {
-//        Main m = new Main();
-        assertEquals(25, Main.getNum());
+    void takeFile() throws IOException {
+        String filePth = "C:/Users/user/IdeaProjects/Letters/src/resources/output.txt";
+        assertEquals(filePth, Main.getFilePath());
+    }
+
+    @Test
+    void takeLeters() {
+        int lettersInFile = 0;
+        int lettersInMain  = Main.getTotalLetters();
+        assertEquals(lettersInFile, lettersInMain);
     }
 }
